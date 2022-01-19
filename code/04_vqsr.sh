b4a7bda10d929b178e5245ba03e6fd26523c7f41
AliSaadatV <ali.saadat@epfl.ch>
2022-01-19 11:37:41 +0100

    corrected output inded vqsr directory

--- code/04_vqsr.sh
+++ code/04_vqsr.sh
@@ -20,21 +20,21 @@ mkdir -p ${OUTPUT_DIR}
 mkdir -p ${TEMP_DIR}
 
 # Run VQSR
-$GATK --java-options "-Djava.io.tmpdir=${TEMP_DIR} -Xms45G -Xmx50G -XX:ParallelGCThreads=2" VariantRecalibrator \
-	-tranche 100.0 -tranche 99.95 -tranche 99.9 \
-	-tranche 99.5 -tranche 99.0 -tranche 97.0 -tranche 96.0 \
-	-tranche 95.0 -tranche 94.0 \
-	-tranche 93.5 -tranche 93.0 -tranche 92.0 -tranche 91.0 -tranche 90.0 \
-	-R $REF \
-	-V ${INPUT_DIR}/merged.vcf.gz \
-	--resource:hapmap,known=false,training=true,truth=true,prior=15.0 \
-	${KNOWN_SITES}/hapmap_3.3.hg38.vcf.gz \
-	--resource:omni,known=false,training=true,truth=false,prior=12.0 \
-	${KNOWN_SITES}/1000G_omni2.5.hg38.vcf.gz \
-	--resource:1000G,known=false,training=true,truth=false,prior=10.0 \
-	${KNOWN_SITES}/1000G_phase1.snps.high_confidence.hg38.vcf.gz \
-	-an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
-	-mode SNP -O ${OUTPUT_DIR}/merged_snp1.recal --tranches-file ${OUTPUT_DIR}/output_snp1.tranches && \
+#$GATK --java-options "-Djava.io.tmpdir=${TEMP_DIR} -Xms45G -Xmx50G -XX:ParallelGCThreads=2" VariantRecalibrator \
+#	-tranche 100.0 -tranche 99.95 -tranche 99.9 \
+#	-tranche 99.5 -tranche 99.0 -tranche 97.0 -tranche 96.0 \
+#	-tranche 95.0 -tranche 94.0 \
+#	-tranche 93.5 -tranche 93.0 -tranche 92.0 -tranche 91.0 -tranche 90.0 \
+#	-R $REF \
+#	-V ${INPUT_DIR}/merged.vcf.gz \
+#	--resource:hapmap,known=false,training=true,truth=true,prior=15.0 \
+#	${KNOWN_SITES}/hapmap_3.3.hg38.vcf.gz \
+#	--resource:omni,known=false,training=true,truth=false,prior=12.0 \
+#	${KNOWN_SITES}/1000G_omni2.5.hg38.vcf.gz \
+#	--resource:1000G,known=false,training=true,truth=false,prior=10.0 \
+#	${KNOWN_SITES}/1000G_phase1.snps.high_confidence.hg38.vcf.gz \
+#	-an QD -an MQ -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
+#	-mode SNP -O ${OUTPUT_DIR}/merged_snp1.recal --tranches-file ${OUTPUT_DIR}/output_snp1.tranches && \
 
 $GATK --java-options "-Djava.io.tmpdir=${TEMP_DIR} -Xms45G -Xmx50G -XX:ParallelGCThreads=2" VariantRecalibrator \
     -tranche 100.0 -tranche 99.95 -tranche 99.9 \
@@ -48,7 +48,7 @@ $GATK --java-options "-Djava.io.tmpdir=${TEMP_DIR} -Xms45G -Xmx50G -XX:ParallelG
     --resource:dbsnp,known=true,training=false,truth=false,prior=2.0 \
     ${KNOWN_SITES}/dbsnp_146.hg38.vcf.gz \
 	-an QD -an MQRankSum -an ReadPosRankSum -an FS -an SOR \
-    -mode INDEL -O ${OUTPUT}/merged_indel1.recal --tranches-file ${OUTPUT}/output_indel1.tranches && \
+    -mode INDEL -O ${OUTPUT_DIR}/merged_indel1.recal --tranches-file ${OUTPUT_DIR}/output_indel1.tranches && \
 
 # Apply VQSR
 $GATK --java-options "-Djava.io.tmpdir=${TEMP_DIR} -Xms45G -Xmx50G -XX:ParallelGCThreads=2" ApplyVQSR \
