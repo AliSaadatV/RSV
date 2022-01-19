9e9f154a7223fe05ffde8f7fd8cba21b481fd216
AliSaadatV <ali.saadat@epfl.ch>
2022-01-19 12:56:03 +0100

    added 06 and 07

---
+++ code/06_pre_annotation.sh
@@ -0,0 +1,64 @@
+#!/bin/bash 
+#SBATCH --chdir /scratch/saadat/RSV
+#SBATCH --nodes 1
+#SBATCH --ntasks 1
+#SBATCH --cpus-per-task 20
+#SBATCH --mem 20G
+#SBATCH --time 03:00:00
+#SBATCH --mail-user=ali.saadat@epfl.ch
+#SBATCH -J pre_annotation_RSV
+#SBATCH --mail-type=END
+#SBATCH -o ./log/pre_annotation_%J.out # Standard output
+#SBATCH -e ./log/pre_annotation_%J.err # Standard error
+set -e
+echo "START AT $(date)"
+
+# Tools
+GATK=/work/gr-fe/saadat/tools/gatk/gatk-4.2.2.0/gatk
+BCFTOOLS=/work/gr-fe/saadat/tools/bcftools/installation/bin/bcftools
+VCFTOOLS=/work/gr-fe/saadat/tools/vcftools/installation/bin/vcftools
+VT=/work/gr-fe/saadat/tools/vt/vt/vt
+
+# Path
+WORK=/work/gr-fe/saadat/RSV
+SCRATCH=/scratch/saadat/pri/second_try
+INPUT_DIR=${WORK}/processed_data/genotype_refinement_output
+OUTPUT_DIR=${WORK}/processed_data/pre_annotation_output
+REF=/work/gr-fe/saadat/Reference_Genome/GRCH38_no_alt/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa.gz
+
+# Create directories
+mkdir -p ${SCRATCH}/temp/${SLURM_JOBID}/io
+
+cd ${OUTPUT_DIR}
+# Remove Batch Effect
+$BCFTOOLS filter -i 'QUAL>=25 & INFO/DP>=20 & FORMAT/DP>=10 & FORMAT/GQ>=20' -S . -Ov \
+	-o bcftools.vcf --threads 20 \
+	${INPUT_DIR}/indel_recal_95_snp_recal_99.7_refined_GQ20.vcf.gz
+bgzip -@ 20 -c bcftools.vcf > bcftools.vcf.gz
+tabix -p vcf bcftools.vcf.gz
+rm bcftools.vcf 
+
+# Exclude filtered
+$GATK --java-options "-Djava.io.tmpdir=${SCRATCH}/temp/${SLURM_JOBID}/io -Xms18G -Xmx18G -XX:ParallelGCThreads=4" \
+        SelectVariants \
+        -V ${OUTPUT_DIR}/bcftools.vcf.gz \
+        -R $REF \
+        --exclude-filtered \
+        -exclude-non-variants \
+        --remove-unused-alternates \
+        --set-filtered-gt-to-nocall \
+        -O ${OUTPUT_DIR}/bcftools_gatk.vcf.gz
+
+# Exclude too variants with minor_AC<=1
+$VCFTOOLS --gzvcf bcftools_gatk.vcf.gz --mac 2 --recode --recode-INFO-all --out bcftools_gatk_vcftools
+bgzip -@ 20 -c bcftools_gatk_vcftools.recode.vcf > bcftools_gatk_vcftools.vcf.gz
+tabix -p vcf bcftools_gatk_vcftools.vcf.gz
+
+# Normalize and break multiallelic 
+$VT decompose -s -o temp.vcf bcftools_gatk_vcftools.vcf.gz
+$VT normalize -r $REF -o bcftools_gatk_vcftools_norm.vcf temp.vcf
+bgzip -@ 20 -c bcftools_gatk_vcftools_norm.vcf > bcftools_gatk_vcftools_norm.vcf.gz
+tabix -p vcf bcftools_gatk_vcftools_norm.vcf.gz
+rm temp.vcf
+
+echo "FINISH AT $(date)"
---
+++ code/07_annotation.sh
@@ -0,0 +1,72 @@
+#!/bin/bash 
+#SBATCH --chdir /scratch/saadat/RSV
+#SBATCH --nodes 1
+#SBATCH --ntasks 10
+#SBATCH --cpus-per-task 1
+#SBATCH --mem 30G
+#SBATCH --time 03:00:00
+#SBATCH --mail-user=ali.saadat@epfl.ch
+#SBATCH -J anno_RSV
+#SBATCH --mail-type=END
+#SBATCH -o ./log/anno_RSV_%J.out # Standard output
+#SBATCH -e ./log/anno_RSV_%J.err # Standard error
+echo "START AT $(date)"
+set -e
+
+# Path
+WORK=/work/gr-fe/saadat/RSV
+REF=/work/gr-fe/saadat/Reference_Genome/GRCH38_no_alt/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa.gz
+SCRATCH=/scratch/saadat/RSV
+CACHE=/work/gr-fe/databases/vep_hg38/cache_hg38_ensembl
+PLUGINS=/work/gr-fe/databases/vep_hg38/plugins
+INPUT_DIR=/work/gr-fe/saadat/RSV/pre_annotation_output
+OUTPUT_DIR=/work/gr-fe/saadat/RSV/annotation_output
+LOFTEE=/work/gr-fe/databases/vep_hg38/loftee
+ANNOVAR=/work/gr-fe/saadat/tools/annovar/annovar
+SLIVAR_DIR=/work/gr-fe/saadat/tools/slivar
+
+# Create output folder
+mkdir -p ${OUTPUT_DIR}
+
+# Activate vep from conda
+eval "$(conda shell.bash hook)"
+conda activate ensembl-vep
+
+vep \
+--offline --vcf \
+--fasta $REF \
+--cache \
+--dir_cache $CACHE \
+--canonical \
+--hgvs \
+--symbol \
+--mane \
+--flag_pick \
+--cache_version 104 \
+--dir_plugins ${LOFTEE}/plugin_hg38/loftee \
+--plugin LoF,loftee_path:${LOFTEE}/plugin_hg38/loftee,human_ancestor_fa:${LOFTEE}/data_hg38/human_ancestor.fa.gz,gerp_bigwig:${LOFTEE}/data_hg38/gerp_conservation_scores.homo_sapiens.GRCh38.bw \
+-i ${INPUT_DIR}/bcftools_gatk_vcftools_norm.vcf.gz \
+-o ${OUTPUT_DIR}/vep.vcf.gz \
+--stats_file ${OUTPUT_DIR}/vep.html \
+--fork 10 \
+--force_overwrite
+
+# Filter with Slivar
+${SLIVAR_DIR}/slivar expr \
+--js ${SLIVAR_DIR}/slivar-functions.js \
+--vcf ${OUTPUT_DIR}/vep.vcf.gz \
+--info 'INFO.impactful && variant.FILTER == "PASS"' \
+--out-vcf ${OUTPUT_DIR}/vep_slivar.vcf
+
+# Annotate with annovar
+perl ${ANNOVAR}/table_annovar.pl ${OUTPUT_DIR}/vep_slivar.vcf ${ANNOVAR}/humandb_hg38/ \
+ -buildver hg38 \
+ -thread 10 \
+ -out ${OUTPUT_DIR}/vep_slivar_annovar.vcf \
+ -vcfinput \
+ -remove \
+ -protocol refGene,ensGene,dbnsfp42a,avsnp147,clinvar_20210501,esp6500siv2_all,gnomad30_genome,exac03,ALL.sites.2015_08,dbscsnv11 \
+ -operation g,g,f,f,f,f,f,f,f,f \
+ -nastring .
+
+echo "END AT $(date)"                                                                                                                                                    1,1           Top
