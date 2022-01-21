858d86d90832dfe57df27b37f305177818f90688
AliSaadatV <ali.saadat@epfl.ch>
2022-01-21 11:56:56 +0100

    filter 50% callrate

--- code/06_pre_annotation.sh
+++ code/06_pre_annotation.sh
@@ -33,33 +33,33 @@ mkdir -p ${OUTPUT_DIR}
 cd ${OUTPUT_DIR}
 # Remove Batch Effect
 $BCFTOOLS filter -i 'QUAL>=25 & INFO/DP>=20 & FORMAT/DP>=10 & FORMAT/GQ>=20' -S . -Ov \
-	-o bcftools.vcf --threads 20 \
-	${INPUT_DIR}/indel_recal_95_snp_recal_99.7_refined_GQ20.vcf.gz
-bgzip -@ 20 -c bcftools.vcf > bcftools.vcf.gz
-tabix -p vcf bcftools.vcf.gz
-rm bcftools.vcf 
+#	-o bcftools.vcf --threads 20 \
+#	${INPUT_DIR}/indel_recal_95_snp_recal_99.7_refined_GQ20.vcf.gz
+#bgzip -@ 20 -c bcftools.vcf > bcftools.vcf.gz
+#tabix -p vcf bcftools.vcf.gz
+#rm bcftools.vcf 
 
 # Exclude filtered
-$GATK --java-options "-Djava.io.tmpdir=${SCRATCH}/temp/${SLURM_JOBID}/io -Xms18G -Xmx18G -XX:ParallelGCThreads=4" \
-        SelectVariants \
-        -V ${OUTPUT_DIR}/bcftools.vcf.gz \
-        -R $REF \
-        --exclude-filtered \
-        -exclude-non-variants \
-        --remove-unused-alternates \
-        --set-filtered-gt-to-nocall \
-        -O ${OUTPUT_DIR}/bcftools_gatk.vcf.gz
+#$GATK --java-options "-Djava.io.tmpdir=${SCRATCH}/temp/${SLURM_JOBID}/io -Xms18G -Xmx18G -XX:ParallelGCThreads=4" \
+#        SelectVariants \
+#        -V ${OUTPUT_DIR}/bcftools.vcf.gz \
+#        -R $REF \
+#        --exclude-filtered \
+#        -exclude-non-variants \
+#        --remove-unused-alternates \
+#        --set-filtered-gt-to-nocall \
+#        -O ${OUTPUT_DIR}/bcftools_gatk.vcf.gz
 
-# Exclude too variants with minor_AC<=1
-$VCFTOOLS --gzvcf bcftools_gatk.vcf.gz --mac 2 --recode --recode-INFO-all --out bcftools_gatk_vcftools
-bgzip -@ 20 -c bcftools_gatk_vcftools.recode.vcf > bcftools_gatk_vcftools.vcf.gz
-tabix -p vcf bcftools_gatk_vcftools.vcf.gz
+# Exclude too variants with minor_AC<=1 and call-rate 50%
+$VCFTOOLS --gzvcf bcftools_gatk.vcf.gz --mac 2 --max-missing 0.5 --recode --recode-INFO-all --out bcftools_gatk_vcftools50
+bgzip -@ 20 -c bcftools_gatk_vcftools50.recode.vcf > bcftools_gatk_vcftools50.vcf.gz
+tabix -p vcf bcftools_gatk_vcftools.vcf50.gz
 
 # Normalize and break multiallelic 
-$VT decompose -s -o temp.vcf bcftools_gatk_vcftools.vcf.gz
-$VT normalize -r $REF -o bcftools_gatk_vcftools_norm.vcf temp.vcf
-bgzip -@ 20 -c bcftools_gatk_vcftools_norm.vcf > bcftools_gatk_vcftools_norm.vcf.gz
-tabix -p vcf bcftools_gatk_vcftools_norm.vcf.gz
+$VT decompose -s -o temp.vcf bcftools_gatk_vcftools50.vcf.gz
+$VT normalize -r $REF -o bcftools_gatk_vcftools50_norm.vcf temp.vcf
+bgzip -@ 20 -c bcftools_gatk_vcftools50_norm.vcf > bcftools_gatk_vcftools50_norm.vcf.gz
+tabix -p vcf bcftools_gatk_vcftools50_norm.vcf.gz
 rm temp.vcf
 
 echo "FINISH AT $(date)"
